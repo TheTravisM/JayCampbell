c85c2d9c944bcd1e7547fa383393629dcc0e609b
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2020-01-20 13:01:17 -0500

    All files

---
+++ footer.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+	<footer id="colophon" class="jc-footer">
+		<nav class="jc-footer-nav">
+			<ul class="jc-footer-nav_ul">
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Start Here
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						FAQ's
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Blog
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Podcast
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Store
+					</a>
+				</li>
+			</ul>
+			<ul class="jc-footer-nav_ul">
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Support
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Resources
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Partners
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Podcast Guests
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Appearances
+					</a>
+				</li>
+			</ul>
+			<ul class="jc-footer-nav_ul">
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Terms of Use
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Privacy Policy
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Return Policy
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<a class="jc-footer-nav_link" href="#">
+						Disclosure
+					</a>
+				</li>
+				<li class="jc-footer-nav_li">
+					<span class="jc-footer_copy-right">
+						Copyright (c) 2020 Jay C. Campbell
+					<span>
+				</li>
+			</ul>
+		</nav>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<!-- <?php // get_template_part( 'template-parts/footer/info' ); ?> -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
